856dfdbb95abc40beb9ccef4891bedb3b8d2ac90
andrew055 <andrijalbrext@gmail.com>
2023-04-10 21:55:58 +0300

    fixed bug with empty input

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-document.getElementById("add").onclick= () => {
+document.getElementById("add").onclick = () => {
     let li = document.createElement("li");
-    let text=document.getElementById("taskAdd").value;
-    let t=document.createTextNode(text);
+    let text = document.getElementById("taskAdd").value;
+
+    if( text == "" )
+    {
+        alert("Будь ласка, введіть задачу")
+        return;
+    }
+    let t = document.createTextNode(text);
+
     li.appendChild(t);
+
     document.getElementById("myUL").appendChild(li);
-    document.getElementById("taskAdd").value="";
+    document.getElementById("taskAdd").value = "";
 }
