c41ecf1c127a0bf92f3fb7ef4e0f2b010283ff07
IvanOvchynnikov <ovchinic76@gmail.com>
2023-04-11 18:42:37 +0300

    Close Button Event

--- script.js
+++ script.js
@@ -1,16 +1,56 @@
+
+let tasks=[]
+
+
 document.getElementById("add").onclick = () => {
     let li = document.createElement("li");
     let text = document.getElementById("taskAdd").value;
 
-    if( text == "" )
-    {
+    if( text == "" ) {
         alert("Будь ласка, введіть задачу")
         return;
     }
-    let t = document.createTextNode(text);
+    let t=document.createTextNode(text);
+    let cur_task=document.createElement("li");
+    cur_task.setAttribute("class","task");
+    cur_task.appendChild(t);
 
-    li.appendChild(t);
+    let ButtonClose=document.createElement("button");
+    ButtonClose.innerHTML="X";
+    ButtonClose.setAttribute("class","buttonClose");
+    ButtonClose.setAttribute("id",`${tasks.length}`);
+    ButtonClose.setAttribute("onclick","close_click(this)");
+    cur_task.appendChild(ButtonClose);
 
-    document.getElementById("myUL").appendChild(li);
+    tasks.push(cur_task);
     document.getElementById("taskAdd").value = "";
+    document.getElementById("myUL").appendChild(cur_task);
+}
+
+function close_click(button){
+    let index=button.id;
+    tasks.splice(index,1);
+    let lists=document.getElementsByClassName("task");
+    while(lists.length>0){
+        lists.item(0).remove();
+    }
+    console.log(tasks)
+    for(let i=0; i<tasks.length; i++)
+    {
+        let t=document.createTextNode(tasks[i].childNodes[0].data);
+        let cur_task=document.createElement("li");
+        cur_task.setAttribute("class","task");
+        cur_task.appendChild(t);
+
+        let ButtonClose=document.createElement("button");
+        ButtonClose.innerHTML="X";
+        ButtonClose.setAttribute("class","buttonClose");
+        ButtonClose.setAttribute("id",`${i}`);
+        ButtonClose.setAttribute("onclick","close_click(this)");
+        cur_task.appendChild(ButtonClose);
+
+        document.getElementById("myUL").appendChild(cur_task);
+    }
+
+
 }
